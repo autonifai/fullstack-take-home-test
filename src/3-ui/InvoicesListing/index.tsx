f0d5fc9c33af5cbec8138b35bad21f1dcd548d21
Marcos Vinícius Correia da Silva <marvinc.silva@gmail.com>
2024-03-01 17:47:32 +0100

    chore: prettify

--- src/3-ui/InvoicesListing/index.tsx
+++ src/3-ui/InvoicesListing/index.tsx
@@ -3,15 +3,17 @@ import useReadInvoices from '../../2-capabilities/use-read-invoices';
 import InvoicesTable from './InvoicesTable';
 import AsyncStatuses from '../../2-capabilities/async-statuses.type';
 
+import styles from './InvoicesListing.module.scss';
+
 type Props = {
   read?: () => { status: AsyncStatuses };
 };
 
 function Loading() {
-  return <>Loading</>;
+  return <div className={styles.centered}>Loading</div>;
 }
 function Failure() {
-  return <>An error has happened =/</>;
+  return <div className={styles.centered}>An error has happened =/</div>;
 }
 
 function InvoicesListing({ read = useReadInvoices }: Props) {
