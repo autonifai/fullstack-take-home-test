a94a0c5c5f603feb683142c1e4b350b28e6a6585
Dario Fernandez <dariofedes@protonmail.com>
2024-03-10 19:32:33 +0100

    add createComponent script

---
+++ createComponent.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ComponentName="$1"
+
+if [ -z "$ComponentName" ]; then
+  echo "Error: a component name must be provided."
+  exit 1
+fi
+
+mkdir -p "./src/$ComponentName"
+
+echo "import $ComponentName from './$ComponentName';
+
+export {
+  $ComponentName
+}" > "./src/$ComponentName/index.ts"
+
+touch "./src/$ComponentName/$ComponentName.sass"
+
+echo "export default function $ComponentName() {
+  return (
+    
+  );
+}" > "./src/$ComponentName/$ComponentName.tsx"
+
+echo "Component $ComponentName succesfully created."
