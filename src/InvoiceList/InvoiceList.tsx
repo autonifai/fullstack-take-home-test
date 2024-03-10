5dc14296e02518aadf48a6e0e4f74b99c32b54c6
Dario Fernandez <dariofedes@protonmail.com>
2024-03-10 20:14:48 +0100

    change InvoiceList list fron unordered to ordered

--- src/InvoiceList/InvoiceList.tsx
+++ src/InvoiceList/InvoiceList.tsx
@@ -6,12 +6,12 @@ export default function InvoiceList({ className }: InvoiceListProps) {
   return (
     <>
       <TableHeader className='invoice-list__header'/>
-      <ul className={`invoice-list ${className}`}>
+      <ol className={`invoice-list ${className}`}>
         <InvoiceListItem className='invoicelist__invoice-list-item' />
         <InvoiceListItem className='invoicelist__invoice-list-item' />
         <InvoiceListItem className='invoicelist__invoice-list-item' />
         <InvoiceListItem className='invoicelist__invoice-list-item' />
-      </ul>
+      </ol>
     </>
   )
 }
