c8485dac14f3b4a0f381500b20f1b6e3099d288a
Dario Fernandez <dariofedes@protonmail.com>
2024-03-16 13:16:29 +0100

    fix invoice list header labels literals

--- src/Invoice/ui/InvoiceList/InvoiceListHeader/InvoiceListHeader.tsx
+++ src/Invoice/ui/InvoiceList/InvoiceListHeader/InvoiceListHeader.tsx
@@ -4,10 +4,10 @@ import './InvoiceListHeader.sass'
 export default function InvoiceListHeader({ className }: InvoiceListHeaderPros) {
   return (
     <ListRow className={`invoice-list-header ${className}`}>
-      <p className='invoice-list-header__column-label'>Invoice name</p>
-      <p className='invoice-list-header__column-label'>Vendor name</p>
-      <p className='invoice-list-header__column-label'>Invoice description</p>
-      <p className='invoice-list-header__column-label'>Due date</p>
+      <p className='invoice-list-header__column-label'>Invoice Number</p>
+      <p className='invoice-list-header__column-label'>Vendor Name</p>
+      <p className='invoice-list-header__column-label'>Invoice Description</p>
+      <p className='invoice-list-header__column-label'>Due Date</p>
       <p className='invoice-list-header__column-label'>Amount</p>
     </ListRow>
   )
