dda89e466cfb2697b85958f782185e8c86808cc9
Fabio William Conceição <messhias@gmail.com>
2024-03-16 00:03:32 +0000

    redux + api fetch

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,9 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import uiSlice from "../features/uiSlice";
+import invoicesSlice from "../features/invoicesSlice";
 
-export default configureStore({
+
+
+const store = configureStore({
 	reducer: {
 		ui: uiSlice,
+		invoices: invoicesSlice
 	},
 })
 
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+export default store
