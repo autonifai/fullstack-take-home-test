d692823f04ca5b8578ca879757ab10fc8f7386c2
Dario Fernandez <dariofedes@protonmail.com>
2024-03-18 01:28:02 +0100

    add Router context for tests that need it

--- src/tests/Invoice/ui/InvoiceList/LineInvoice/LineInvoice.spec.tsx
+++ src/tests/Invoice/ui/InvoiceList/LineInvoice/LineInvoice.spec.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import LineInvoice from '../../../../../Invoice/ui/InvoiceList/LineInvoice/LineInvoice';
 import invoiceFixture from '../../../../Fixtures/invoiceFixture';
 
 describe('LineInvoice', () => {
   it('should display the name, vendor and description', () => {
-    render(<LineInvoice invoice={invoiceFixture} />)
+    render(
+      <MemoryRouter>
+        <LineInvoice invoice={invoiceFixture} />
+      </MemoryRouter>
+    )
 
     const name = screen.getByText(invoiceFixture.number)
     const vendor = screen.getByText(invoiceFixture.vendor)
@@ -18,7 +23,11 @@ describe('LineInvoice', () => {
   it('should display the due date properly formatted', () => {
     invoiceFixture.dueDate = "01/01/2024"
 
-    render(<LineInvoice invoice={invoiceFixture} />)
+    render(
+      <MemoryRouter>
+        <LineInvoice invoice={invoiceFixture} />
+      </MemoryRouter>
+    )
 
     const dueDate = screen.getByText('01/01/24')
 
@@ -28,7 +37,11 @@ describe('LineInvoice', () => {
   it('should display the ammount properly formatted', () => {
     invoiceFixture.totalAmount = 16000.0
 
-    render(<LineInvoice invoice={invoiceFixture} />)
+    render(
+      <MemoryRouter>
+        <LineInvoice invoice={invoiceFixture} />
+      </MemoryRouter>
+    )
 
     const dueDate = screen.getByText('$16,000.00')
 
